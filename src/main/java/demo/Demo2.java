4a350c2a34d2ac999ade3e7ebb39f5622fbeed52
tom_glb <goodMorning_glb@163.com>
2020-05-19 15:31:03 +0800

    modify

--- src/main/java/demo/Demo2.java
+++ src/main/java/demo/Demo2.java
@@ -2,6 +2,8 @@ package demo;
 
 public class Demo2 {
    public static void main(String[] args) {
-	  System.out.println("我不相信任正非获得很艰辛");
+	  System.out.println("我不相信任正非活的很艰辛");
+	  System.out.println("但王思聪可能很艰辛");
+	  System.out.println("谁会相信了????");
    }
 }
